2344bf617af924d4aeb206bdaecfcbdc7fa20f18
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2021-02-16 21:17:38 +0100

    Initial commit

---
+++ database/migrations/2021_02_16_142757_create_image_models_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImageModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_model', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_models');
+    }
+}
